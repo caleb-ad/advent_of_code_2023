633fbbc4e641e9a70238b0466e9a7000f343c8a0
caleb dennis <caleb.dennis429@gmail.com>
2023-12-07 00:48:15 -0700

    day 7 part 2

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,7 +1,6 @@
 use std::io::Read;
-use std::collections::HashMap;
 
-const RANKING: &'static [u8] = &[b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'T', b'J', b'Q', b'K', b'A'];
+const RANKING: &'static [u8] = &[b'J', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'T', b'Q', b'K', b'A'];
 const RBASE: u32 = RANKING.len() as u32;
 const HAND_SIZE: u32 = 5;
 
@@ -17,15 +16,35 @@ impl<'a> Hand<'a> {
         let mut digit = HAND_SIZE;
         let mut matches = [0u32; RBASE as usize];
         let mut strength: u32 = 0;
+        let mut joker_count = 0;
+        let mut joker_mask = 0;
+
         for c in hand.bytes() {
             let cval = RANKING.iter().position(|cp| *cp == c).unwrap();
             //1 3 7 15 31
             //5 6 7 9 10 16 31
-            matches[cval] += matches[cval] + 1;
+            if c != b'J' { matches[cval] += matches[cval] + 1 }
+            else {
+                joker_count += 1;
+                joker_mask = (joker_mask << 1) | 1;
+            }
             digit -= 1;
             strength += (cval as u32) << (digit * 4);
         }
-        strength += matches.iter().sum::<u32>() << (HAND_SIZE * 4);
+
+        // make best hand possible with jokers
+        let mut matches_sum = 0;
+        let mut max = 0;
+        for dup in matches {
+            if dup > max {
+                matches_sum += max;
+                max = dup;
+            } else { matches_sum += dup; }
+        }
+        matches_sum = matches_sum + max*(1 << joker_count) + joker_mask;
+
+        strength += matches_sum << (HAND_SIZE * 4);
+        // println!("{:x}", strength);
         Hand { hand, strength: strength, bid: bid}
     }
 }
@@ -58,7 +77,10 @@ mod tests {
 
     #[test]
     fn test_hand_strengths() {
-        let _h = Hand::new("AA8AA", 1);
-        let _h = Hand::new("QQQQ2", 1);
+        let _h = Hand::new("KJTJT", 1);
+        let _h = Hand::new("JJ8J3", 1);
+        let _h = Hand::new("JJ66J", 1);
+        let _h = Hand::new("JJJJJ", 1);
+        let _h = Hand::new("22222", 1);
     }
 }
