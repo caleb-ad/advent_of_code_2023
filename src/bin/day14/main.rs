8e954d3018dede12d2f76400f86a17873ca04486
Caleb Dennis <caleb.dennis429@gmail.com>
2023-12-20 23:14:03 -0800

    day14 part2: Hashing solution

--- src/bin/day14/main.rs
+++ src/bin/day14/main.rs
@@ -1,12 +1,65 @@
+use std::hash::Hasher;
 use std::io::Read;
+use std::ops::{Index, IndexMut};
+use ahash::{AHasher, HashMap, HashMapExt};
+use std::hash::Hash;
 
-type Grid = Vec<Vec<u8>>;
+struct Grid {
+    g: Vec<u8>,
+    dim: (usize, usize) // (width, height)
+}
+
+impl Index<usize> for Grid {
+    type Output = [u8];
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.g[index * self.dim.0..(index + 1) * self.dim.0]
+    }
+}
+
+impl IndexMut<usize> for Grid {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.g[index * self.dim.0..(index + 1) * self.dim.0]
+    }
+}
+
+impl std::fmt::Debug for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for l in self.lines() {
+            unsafe { write!(f, "{:?}\n", std::str::from_utf8_unchecked(l))? }
+        }
+        write!(f, "")
+    }
+}
+
+impl Grid {
+    fn from_input(b: &[u8]) -> Self {
+        let mut g = Vec::new();
+        let width = b.iter().position(|b| *b == b'\n').expect("");
+        for line in b.split(|b| *b == b'\n') {
+            g.extend_from_slice(line);
+        }
+        Grid{g, dim: (width, b.len() / width)}
+    }
+
+    fn lines(&self) -> impl DoubleEndedIterator + Iterator<Item=&[u8]> {
+        self.g.chunks(self.dim.0)
+    }
+
+    fn hash(&self) -> u64 {
+        let mut hasher = AHasher::default();
+        self.g.as_slice().hash(&mut hasher);
+        hasher.finish()
+    }
+
+    fn width(&self) -> usize { self.dim.0 }
+    fn height(&self) -> usize { self.dim.1 }
+}
 
 fn main() {
     let mut input = vec![];
     let _ = std::fs::File::open("C:\\Users\\caleb\\Documents\\Projects\\advent_of_code_2023\\src\\bin\\day14\\input.txt").expect("").read_to_end(&mut input);
 
-    let mut grid: Grid = input.split(|b| *b == b'\n').map(|s| s.to_vec()).collect();
+    let mut grid = Grid::from_input(&input);
 
     // test load = 153
     spin_cycle(&mut grid, 1000_000_000);
@@ -15,10 +68,10 @@ fn main() {
 
 fn roll_north(grid: &mut Grid) {
     let mut block = Vec::new();
-    block.resize(grid[0].len(), 0);
+    block.resize(grid.width(), 0);
 
-    for yidx in 0..grid.len() {
-        for ( xidx, block) in (0..grid[yidx].len()).zip(block.iter_mut()) {
+    for yidx in 0..grid.height() {
+        for ( xidx, block) in (0..grid.width()).zip(block.iter_mut()) {
             let current = grid[yidx][xidx];
             if current == b'O' && *block < yidx {
                 grid[*block][xidx] = b'O';
@@ -32,10 +85,10 @@ fn roll_north(grid: &mut Grid) {
 
 fn roll_west(grid: &mut Grid) {
     let mut block = Vec::new();
-    block.resize(grid.len(), 0);
+    block.resize(grid.height(), 0);
 
-    for xidx in 0..grid[0].len() {
-        for (yidx, block) in (0..grid.len()).zip(block.iter_mut()) {
+    for xidx in 0..grid.width() {
+        for (yidx, block) in (0..grid.height()).zip(block.iter_mut()) {
             let current = grid[yidx][xidx];
             if current == b'O' && *block < xidx {
                 grid[yidx][*block] = b'O';
@@ -49,10 +102,10 @@ fn roll_west(grid: &mut Grid) {
 
 fn roll_east(grid: &mut Grid) {
     let mut block = Vec::new();
-    block.resize(grid.len(), grid[0].len());
+    block.resize(grid.height(), grid.width());
 
-    for xidx in (0..grid[0].len()).rev() {
-        for (yidx, block) in (0..grid.len()).zip(block.iter_mut()) {
+    for xidx in (0..grid.width()).rev() {
+        for (yidx, block) in (0..grid.height()).zip(block.iter_mut()) {
             let current = grid[yidx][xidx];
             if current == b'O' && *block > xidx + 1 {
                 grid[yidx][*block - 1] = b'O';
@@ -67,10 +120,10 @@ fn roll_east(grid: &mut Grid) {
 
 fn roll_south(grid: &mut Grid) {
     let mut block = Vec::new();
-    block.resize(grid[0].len(), grid.len());
+    block.resize(grid.width(), grid.height());
 
-    for yidx in (0..grid.len()).rev() {
-        for ( xidx, block) in (0..grid[yidx].len()).zip(block.iter_mut()) {
+    for yidx in (0..grid.height()).rev() {
+        for ( xidx, block) in (0..grid.width()).zip(block.iter_mut()) {
             let current = grid[yidx][xidx];
             if current == b'O' && *block > yidx + 1 {
                 grid[*block - 1][xidx] = b'O';
@@ -83,31 +136,38 @@ fn roll_south(grid: &mut Grid) {
 }
 
 fn spin_cycle(grid: &mut Grid, n: usize) {
+    let mut states: HashMap<u64, usize> = HashMap::new();
+    let mut idx = 0;
+    let mut cycle = None;
     for _ in 0..n {
+        if let Some(pidx) = states.insert(grid.hash(), idx) {
+            cycle = Some(pidx);
+            break;
+        }
         roll_north(grid);
         roll_west(grid);
         roll_south(grid);
         roll_east(grid);
+        idx += 1;
+    }
+    if let Some(c) = cycle {
+        for _ in 0..((n - idx) % (idx - c)) {
+            roll_north(grid);
+            roll_west(grid);
+            roll_south(grid);
+            roll_east(grid);
+        }
     }
 }
 
 fn calculate_load(grid: &Grid) -> usize {
     let mut load = 1;
     let mut sum = 0;
-    for line in grid.iter().rev() {
+    for line in grid.lines().rev() {
         for b in line {
             if *b == b'O' { sum += load }
         }
         load += 1;
     }
     sum
-}
-
-fn print_grid(grid: &Grid) {
-    for line in grid {
-        unsafe {
-            println!("{:?}", std::str::from_utf8_unchecked(&line))
-        }
-    }
-    println!("");
 }
